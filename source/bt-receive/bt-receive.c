cbde46bd1402b4b5ea1acd491270c8f152e28fe9
Brian A Mejorado <bam9523@rit.edu>
2016-12-05 05:44:21 +0000

    Thread functionality into bt-receive
    The receive program now accepts the information it receives and converts
    it into Morse code.

--- source/bt-receive/bt-receive.c
+++ source/bt-receive/bt-receive.c
@@ -3,7 +3,10 @@
 #include <sys/socket.h>
 #include <bluetooth/bluetooth.h>
 #include <bluetooth/rfcomm.h>
+#include <wiringPi.h>
+
 #include "bt-receive/converter.h"
+#include "bt-receive/blink_morse.h"
 
 int main(void)
 {
@@ -11,6 +14,13 @@ int main(void)
     char buf[1024] = { 0 };
     int s, client, bytes_read;
     socklen_t opt = sizeof(rem_addr);
+
+    // create morse code table
+    Morse * codes = build_code_table();
+
+    // prepare wiringPi pin 0 ( GPIO 17 )
+    wiringPiSetup();
+    pinMode( 0, OUTPUT );
 
     // allocate socket
     s = socket(AF_BLUETOOTH, SOCK_STREAM, BTPROTO_RFCOMM);
@@ -29,17 +39,20 @@ int main(void)
     client = accept(s, (struct sockaddr *)&rem_addr, &opt);
 
     ba2str( &rem_addr.rc_bdaddr, buf );
-    fprintf(stderr, "accepted connection from %s\n", buf);
-    memset(buf, 0, sizeof(buf));
+    fprintf(stderr, "Accepted connection from %s\n", buf);
+    memset(buf, 0, sizeof(buf)); // clear buffer for input
 
     // read data from the client
     bytes_read = read(client, buf, sizeof(buf));
     if( bytes_read > 0 ) {
-        printf("received [%s]\n", buf);
+        printf("Received [%s]\n", buf);
+        printf("Morse code translation:\n");
+        blink_signal_from_input( buf, codes );
     }
 
     // close connection
     close(client);
     close(s);
+    destroy_code_table( codes );
     return 0;
 }
