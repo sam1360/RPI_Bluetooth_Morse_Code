7337294d3cf93273e0899993c579c64ea10562c2
Brian A Mejorado <bam9523@rit.edu>
2016-12-08 21:46:40 -0800

    Move bluetooth sending code into a function

--- source/bt-send/bt-send.c
+++ source/bt-send/bt-send.c
@@ -8,10 +8,26 @@
 
 #define MAX_SIZE  500
 
-int main( int argc, char * argv[] )
-{
+
+static void make_connection( int * s, int * status, char * dest ) {
+    struct sockaddr_rc addr = { 0 };
+
+    // allocate a socket
+    *s = socket( AF_BLUETOOTH, SOCK_STREAM, BTPROTO_RFCOMM );
+
+    // set the connection parameters (who to connect to)
+    addr.rc_family = AF_BLUETOOTH;
+    addr.rc_channel = (uint8_t) 1;
+    str2ba( dest, &addr.rc_bdaddr );
+
+    // connect to server
+    *status = connect( *s, (struct sockaddr *)&addr, sizeof(addr) );
+}
+
+int main( int argc, char * argv[] ) {
     const char * usage = "usage: send bt-receive-addr";
-    char buffer[MAX_SIZE];
+    char buffer[ MAX_SIZE ];
+    int s, status;
 
     if( argc != 2  ) {
         fprintf( stderr, "%s\n", usage );
@@ -22,23 +38,12 @@ int main( int argc, char * argv[] )
         return EXIT_FAILURE;
     }
 
-    struct sockaddr_rc addr = { 0 };
-    int s, status;
-    char * dest = argv[1];
+    make_connection( &s, &status, argv[1] );
 
-    // allocate a socket
-    s = socket(AF_BLUETOOTH, SOCK_STREAM, BTPROTO_RFCOMM);
-
-    // set the connection parameters (who to connect to)
-    addr.rc_family = AF_BLUETOOTH;
-    addr.rc_channel = (uint8_t) 1;
-    str2ba( dest, &addr.rc_bdaddr );
-
-    // connect to server
-    status = connect(s, (struct sockaddr *)&addr, sizeof(addr));
-    while(1){
+    while(1) {
         if( status == 0 ) {
-            fgets(buffer, MAX_SIZE, stdin);
+            // collect and send a message
+            fgets( buffer, MAX_SIZE, stdin );
             status = write( s, buffer, MAX_SIZE );
             status = 0;
         }
@@ -47,8 +52,6 @@ int main( int argc, char * argv[] )
         }
 
     }
-    // send a message
-
 
     close(s);
     return EXIT_SUCCESS;
