c5387c1966cfcce9a44b2df7de827844cc42355b
Brian A Mejorado <bam9523@rit.edu>
2016-12-09 07:29:40 -0800

    Update get_index function in converter.c
    This commit replaces the giant switch statement with three if statements
    for readability and efficiency. Now code folding in the editor is no
    longer necessary to reasonably navigate the file!
    
    Since converter.c does not really need the get_index in the public
    interface (since nothing will use it), the prototype has been removed
    from the header file and the function was changed to static.

--- include/bt-receive/converter.h
+++ include/bt-receive/converter.h
@@ -4,14 +4,6 @@
 #include "bt-receive/morse.h"
 
 /**
- * Acquire the index of the code in the table corresponding to the character.
- *
- * @param c  The character to convert.
- * @return   The converted character.
- */
-size_t get_code_index( char c );
-
-/**
  * Builds the set of Morse codes supported by the system.
  *
  * @return  The created table.
--- source/bt-receive/converter.c
+++ source/bt-receive/converter.c
@@ -50,212 +50,44 @@ Morse * build_code_table(void) {
     codes[23] = build_morse( 'x', "-**-", 0x757 ); // 11101010111
     codes[24] = build_morse( 'y', "-*--", 0x1D77 ); // 1110101110111
     codes[25] = build_morse( 'z', "--**", 0x775 ); // 11101110101
-    codes[26] = build_morse( '1', "*----", 0x17777 ); // 10111011101110111
-    codes[27] = build_morse( '2', "**---", 0x5777 ); // 101011101110111
-    codes[28] = build_morse( '3', "***--", 0x1577 ); // 1010101110111
-    codes[29] = build_morse( '4', "****-", 0x557 ); // 10101010111
-    codes[30] = build_morse( '5', "*****", 0x155 ); // 101010101
-    codes[31] = build_morse( '6', "-****", 0x755 ); // 11101010101
-    codes[32] = build_morse( '7', "--***", 0x1DD5 ); // 1110111010101
-    codes[33] = build_morse( '8', "---**", 0x7775 ); // 111011101110101
-    codes[34] = build_morse( '9', "----*", 0x1DDDD ); // 11101110111011101
-    codes[35] = build_morse( '0', "-----", 0x77777 ); // 1110111011101110111
+    codes[26] = build_morse( '0', "-----", 0x77777 ); // 1110111011101110111
+    codes[27] = build_morse( '1', "*----", 0x17777 ); // 10111011101110111
+    codes[28] = build_morse( '2', "**---", 0x5777 ); // 101011101110111
+    codes[29] = build_morse( '3', "***--", 0x1577 ); // 1010101110111
+    codes[30] = build_morse( '4', "****-", 0x557 ); // 10101010111
+    codes[31] = build_morse( '5', "*****", 0x155 ); // 101010101
+    codes[32] = build_morse( '6', "-****", 0x755 ); // 11101010101
+    codes[33] = build_morse( '7', "--***", 0x1DD5 ); // 1110111010101
+    codes[34] = build_morse( '8', "---**", 0x7775 ); // 111011101110101
+    codes[35] = build_morse( '9', "----*", 0x1DDDD ); // 11101110111011101
     codes[36] = build_morse( ' ', " ", 0x0 );
 
     return codes;
 }
 
-size_t get_code_index(char c) {
+static size_t get_code_index( char c ) {
     size_t result = 36;
-    switch(c){
-        case 'a':
-        case 'A':
-            result = 0;
-            break;
-
-        case 'b':
-        case 'B':
-            result = 1;
-            break;
-
-        case 'c':
-        case 'C':
-            result = 2;
-            break;
-
-        case 'd':
-        case 'D':
-            result = 3;
-            break;
-
-        case 'e':
-        case 'E':
-            result = 4;
-            break;
-
-        case 'f':
-        case 'F':
-            result = 5;
-            break;
-
-        case 'g':
-        case 'G':
-            result = 6;
-            break;
-
-        case 'h':
-        case 'H':
-            result = 7;
-            break;
-
-        case 'i':
-        case 'I':
-            result = 8;
-            break;
-
-        case 'j':
-        case 'J':
-            result = 9;
-            break;
-
-        case 'k':
-        case 'K':
-            result = 10;
-            break;
-
-        case 'l':
-        case 'L':
-            result = 11;
-            break;
-
-        case 'm':
-        case 'M':
-            result = 12;
-            break;
-
-        case 'n':
-        case 'N':
-            result = 13;
-            break;
-
-        case 'o':
-        case 'O':
-            result = 14;
-            break;
-
-        case 'p':
-        case 'P':
-            result = 15;
-            break;
-
-        case 'q':
-        case 'Q':
-            result = 16;
-            break;
-
-        case 'r':
-        case 'R':
-            result = 17;
-            break;
-
-        case 's':
-        case 'S':
-            result = 18;
-            break;
-
-        case 't':
-        case 'T':
-            result = 19;
-            break;
-
-        case 'u':
-        case 'U':
-            result = 20;
-            break;
-
-        case 'v':
-        case 'V':
-            result = 21;
-            break;
-
-        case 'w':
-        case 'W':
-            result = 22;
-            break;
-
-        case 'x':
-        case 'X':
-            result = 23;
-            break;
-
-        case 'y':
-        case 'Y':
-            result = 24;
-            break;
-
-        case 'z':
-        case 'Z':
-            result = 25;
-            break;
-
-        case '1':
-            result = 26;
-            break;
-
-        case '2':
-            result = 27;
-            break;
-
-        case '3':
-            result = 28;
-            break;
-
-        case '4':
-            result = 29;
-            break;
-
-        case '5':
-            result = 30;
-            break;
-
-        case '6':
-            result = 31;
-            break;
-
-        case '7':
-            result = 32;
-            break;
-
-        case '8':
-            result = 33;
-            break;
-
-        case '9':
-            result = 34;
-            break;
-
-        case '0':
-            result = 35;
-            break;
-
-        default:
-            result = 36;
-            break;
+    if( 'a' <= c && 'z' >= c ) { // 'a' <= c <= 'z'
+        result = c - 'a';
     }
-
+    else if( 'A' <= c && 'Z' >= c ) { // 'A' <= c <= 'Z'
+        result = c - 'A';
+    }
+    else if( '0' <= c && '9' >= c ) { // '0' <= c <= '9'
+        result = c - '0' + 26;
+    }
     return result;
 }
 
 Morse * get_codes_from_input( const char * buffer, size_t size, Morse * codes ) {
     Morse * converted = malloc( sizeof(Morse) * size );
     if( converted == NULL ) {
-        fprintf(stderr, "Could not allocate enough space to build the table!\n" );
+        fprintf( stderr, "Could not allocate enough space to build the table!\n" );
         exit(-1);
     }
     for( size_t i = 0; i < size; i++ ) { // index into the code table
         converted[i] = codes[ get_code_index(buffer[i]) ];
-        fprintf( stdout, "%s", converted[i]->convStr );
-        fprintf( stdout, " " );
+        printf( "%s ", converted[i]->convStr );
     }
     printf( "\n" );
     return converted;
